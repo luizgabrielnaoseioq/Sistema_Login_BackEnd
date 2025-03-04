2e0fef594ef85504ec56174a4d31883f6d829f37
Nazax <gabrielon6689@gmail.com>
2025-03-04 12:36:04 -0300

    Atualizei.

--- src/main/java/com/nazax/login_notion/entity/Role.java
+++ src/main/java/com/nazax/login_notion/entity/Role.java
@@ -4,14 +4,17 @@ import com.nazax.login_notion.enums.RoleName;
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "roles")
+@Table(name="roles")
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+@Getter
 public class Role {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
